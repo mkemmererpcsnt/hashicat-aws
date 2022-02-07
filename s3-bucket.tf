a07711994e2d8e618ae00649778e7b2b37f32150
mkemmererpcsnt <mkemmererpcsnt@gmail.com>
2022-02-07 21:58:31 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,14 +1,8 @@
-// module "s3-bucket" {
-//   source  = "app.terraform.io/sev1-michaelkemmerer/s3-bucket/aws"
-//   version = "2.8.0"
-//   # insert required variables here
-// }
-
 module "s3-bucket" {
   source  = "app.terraform.io/sev1-michaelkemmerer/s3-bucket/aws"
-  version = "2.13.0"
-  # insert required variables here
+  version = "2.8.0"
 }
+
 
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
